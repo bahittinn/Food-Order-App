1c6e9a87266748749ff8db7067b71c588381456f
bahittinn <bahittin84@gmail.com>
2023-08-07 22:28:13 +0300

    project created

---
+++ Food-Order/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food-Order
+//
+//  Created by Bahittin on 7.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

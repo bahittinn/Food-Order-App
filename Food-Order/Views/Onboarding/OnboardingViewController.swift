d34843dda070e86289315b199ba1ff15bec8c02d
bahittinn <bahittin84@gmail.com>
2023-08-08 22:47:30 +0300

    onboarding design

--- Food-Order/Views/Onboarding/OnboardingViewController.swift
+++ Food-Order/Views/Onboarding/OnboardingViewController.swift
@@ -19,12 +19,19 @@ class OnboardingViewController: UIViewController {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        slides = [
+            OnboardingSlide(title: "Delicious Dishes", description: "Experience a variety of amazing dishes from different cultures around the world.", image: UIImage(named: "slide1")),
+            OnboardingSlide(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: UIImage(named: "slide2")),
+            OnboardingSlide(title: "Instant World-Wide Delivery", description: "Your orders will be delivered instantly irrespective of your location around the world", image: UIImage(named: "slide3")),
+        ]
+        
     }
     @IBAction func nextButtonClicked(_ sender: UIButton) {
     }
 }
 
-extension OnboardingViewController: UICollectionViewDelegate,UICollectionViewDataSource {
+extension OnboardingViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return slides.count
     }
@@ -34,4 +41,9 @@ extension OnboardingViewController: UICollectionViewDelegate,UICollectionViewDat
         cell.setup(slides[indexPath.row])
         return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
 }

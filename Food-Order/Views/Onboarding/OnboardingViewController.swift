e58f06e52de6a21adb5e49b4988d995173c1b60e
bahittinn <bahittin84@gmail.com>
2023-08-08 23:04:31 +0300

    home viewcontroller

--- Food-Order/Views/Onboarding/OnboardingViewController.swift
+++ Food-Order/Views/Onboarding/OnboardingViewController.swift
@@ -8,13 +8,22 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var pageControl: UIPageControl!
     
     var slides: [OnboardingSlide] = []
-    
+    var currentPage = 0 {
+        didSet {
+            pageControl.currentPage = currentPage
+            if currentPage == slides.count - 1 {
+                nextButton.setTitle("Get Started", for: .normal)
+            } else {
+                nextButton.setTitle("Next", for: .normal)
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
@@ -28,6 +37,17 @@ class OnboardingViewController: UIViewController {
         
     }
     @IBAction func nextButtonClicked(_ sender: UIButton) {
+        
+        if currentPage == slides.count - 1 {
+            let controller = storyboard?.instantiateViewController(withIdentifier: "HomeNC") as! UINavigationController
+            controller.modalPresentationStyle = .fullScreen
+            controller.modalTransitionStyle   = .flipHorizontal
+            present(controller, animated: true)
+        } else {
+            currentPage += 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
     }
 }
 
@@ -46,4 +66,8 @@ extension OnboardingViewController: UICollectionViewDelegate,UICollectionViewDat
         return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
     
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        currentPage = Int(scrollView.contentOffset.x / width)
+    }
 }

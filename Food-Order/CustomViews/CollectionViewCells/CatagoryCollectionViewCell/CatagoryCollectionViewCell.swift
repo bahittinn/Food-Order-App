e0afa1148a08db8d1c5b6549baac871b3acdd34f
bahittinn <bahittin84@gmail.com>
2023-08-09 21:32:57 +0300

    New collectionview Cell

---
+++ Food-Order/CustomViews/CollectionViewCells/CatagoryCollectionViewCell/CatagoryCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CatagoryCollectionViewCell.swift
+//  Food-Order
+//
+//  Created by Bahittin on 9.08.2023.
+//
+
+import UIKit
+
+class CatagoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var catagoryTitleLabel: UILabel!
+    @IBOutlet weak var catagoryImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    func setup(category: DishCategory) {
+        
+    }
+}
---
+++ Food-Order/Models/DishCategory.swift
@@ -0,0 +1,12 @@
+//
+//  DishCatagory.swift
+//  Food-Order
+//
+//  Created by Bahittin on 9.08.2023.
+//
+
+import Foundation
+
+struct DishCategory {
+    let id, name, image: String
+}

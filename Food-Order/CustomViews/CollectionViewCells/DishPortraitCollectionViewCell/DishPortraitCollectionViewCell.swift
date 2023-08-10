6edef806c21e64f268505347ac8673b56437b415
bahittinn <bahittin84@gmail.com>
2023-08-10 21:12:02 +0300

    xib design

---
+++ Food-Order/CustomViews/CollectionViewCells/DishPortraitCollectionViewCell/DishPortraitCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DishPortraitCollectionViewCell.swift
+//  Food-Order
+//
+//  Created by Bahittin on 10.08.2023.
+//
+
+import UIKit
+
+class DishPortraitCollectionViewCell: UICollectionViewCell {
+
+   
+
+}

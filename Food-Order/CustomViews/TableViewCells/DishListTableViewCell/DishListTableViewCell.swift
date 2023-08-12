b23c2fc3220033763bfe3aa0658005b65d44582d
bahittinn <bahittin84@gmail.com>
2023-08-12 23:30:08 +0300

    dish list table view cell

---
+++ Food-Order/CustomViews/TableViewCells/DishListTableViewCell/DishListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DishListTableViewCell.swift
+//  Food-Order
+//
+//  Created by Bahittin on 12.08.2023.
+//
+
+import UIKit
+
+class DishListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+    
+}

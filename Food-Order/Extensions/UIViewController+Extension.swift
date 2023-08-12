05e45114dc5869e354ee96e6af91d20635affdd1
bahittinn <bahittin84@gmail.com>
2023-08-12 22:55:20 +0300

    new extension

---
+++ Food-Order/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Extension.swift
+//  Food-Order
+//
+//  Created by Bahittin on 12.08.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    static func instantiate() -> Self {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: identifier) as! Self
+        return controller
+    }
+}

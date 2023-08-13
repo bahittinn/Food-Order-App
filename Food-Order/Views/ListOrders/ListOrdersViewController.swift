3c1a309d71d3ef24c79a1568ce1f546de4e83850
bahittinn <bahittin84@gmail.com>
2023-08-13 23:10:22 +0300

    List orders view design

---
+++ Food-Order/Views/ListOrders/ListOrdersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ListOrdersViewController.swift
+//  Food-Order
+//
+//  Created by Bahittin on 13.08.2023.
+//
+
+import UIKit
+
+class ListOrdersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
